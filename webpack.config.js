e528b5809e0cf5ddaf59c08448b8bc3ceed83024
Ariel <a.o.hyman@gmail.com>
2019-11-08 17:01:05 -0500

    Reset Environment: Webpack, .babelrc, package.json, .gitignore

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const multi = require('multi-loader');
+
+module.exports = {
+  entry: './client/index.jsx',
+  mode: 'development',
+  devtool: 'eval-source-map',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    //Rules for loaders to handle extensions
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules | bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+        // options: { presets: ['@babel/preset-env'] }
+      },
+      {
+        test: /\.css$/,
+        use: {
+          loader: multi('style-loader', 'css-loader')
+        }
+      }
+    ]
+  },
+  //Required to serve application + create instances of DOM in test environment using JSDOM
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    //HMR on the dev server
+    hot: true,
+    contentBase: path.resolve(__dirname, 'build/bundle.js'),
+    publicPath: '/',
+    //Cors
+    headers: { 'Access-Control-Allow-Origin': '*' }
+  }
+};
